ab1d342e7a3652b73920783116f87149d3134154
bokuwakira1411 <158143866+bokuwakira1411@users.noreply.github.com>
2025-06-06 03:19:01 +0700

    Update QA_knowledge.py

--- Mistral GGUF/basic_prompt_designs/QA_knowledge.py
+++ Mistral GGUF/basic_prompt_designs/QA_knowledge.py
@@ -10,11 +10,13 @@ class QA_knowledge(Pattern):
         self.functions = Global_Function(tokenizer, model, comp_model, X, vectorizer, task_lib)
     @overrides()
     def zero_shot_direct(self, text):
-        return f"Explain the following in simple terms (2-3 sentences):\n{text}"
+        return f"""Instruct: Answer clearly, exactly the following :\n{text}. You can use reliable sources to answer such as google, wikipedia,....\n
+                 Answer: """
 
     @overrides()
     def zero_shot_CoT(self, text):
-        return f"Q: {text}\nA: Let's break it down and then answer."
+        return f"""Instruct:  {text}\n
+                   Answer: Let's break it down and then answer clearly, exactly the following."""
 
     @overrides()
     def zero_shot_CoT_SC(self, text, num_samples=5, max_len=50, do_print=False):
@@ -38,55 +40,53 @@ class QA_knowledge(Pattern):
     @overrides()
     def few_shots_direct(self, text):
         return f"""
-            Answer the following question, using reliable sources such as wikipedia,...
-            Q: "between 1900 and 1920 where did most of the migrants to the united states come from",
-            A: "During the period between 1900 and 1920, most migrants to the United States came from Europe, particularly from Italy, Poland, and Russia. There were also significant numbers of immigrants from Canada, Mexico, and other parts of Latin America. In addition, there were smaller numbers of immigrants from Asia and other parts of the world."
+            Instruct: Answer the following question, using reliable sources such as wikipedia,...Here are some examples:
+            Question:between 1900 and 1920 where did most of the migrants to the united states come from,
+            Answer: During the period between 1900 and 1920, most migrants to the United States came from Europe, particularly from Italy, Poland, and Russia. There were also significant numbers of immigrants from Canada, Mexico, and other parts of Latin America. In addition, there were smaller numbers of immigrants from Asia and other parts of the world.
 
-            Q: "What does inioluwa mean",
-            A: "Inioluwa is a Nigerian name that means \"God's gift\" in Yoruba, a language spoken in Nigeria. It is typically given to a child who is seen as a blessing from God."
+            Question: What does inioluwa mean,
+            Answer: Inioluwa is a Nigerian name that means \"God's gift\" in Yoruba, a language spoken in Nigeria. It is typically given to a child who is seen as a blessing from God.
 
-            Q: "what is CVD",
-            A: "CVD stands for Cardiovascular Disease. It is a group of conditions that affect the heart and blood vessels. These conditions include coronary artery disease (which can lead to heart attacks),"
-
-            Explain the following in simple terms (2-3 sentences):\n{text}. "
+            Question: what is CVD,
+            Answer: CVD stands for Cardiovascular Disease. It is a group of conditions that affect the heart and blood vessels. These conditions include coronary artery disease (which can lead to heart attacks),
+            
+            Now answer clearly, exactly the following: {text}, do not offer irrelevant information.
+            Answer:\n 
 
                 """
     @overrides()
     def few_shots_CoT(self, text):
-        return f"""Answer the question using reliable information. Think step by step.
+        return f"""Instruct: Answer the question using reliable information. Think step by step.
 
-                Q: Between 1900 and 1920, where did most migrants to the United States come from?
-                A: During this period, most migrants to the U.S. came from Europe, especially Italy, Poland, and Russia. Some also came from Canada, Mexico, and parts of Asia.
+                Question:between 1900 and 1920 where did most of the migrants to the united states come from,
+                Answer: During the period between 1900 and 1920, most migrants to the United States came from Europe, particularly from Italy, Poland, and Russia. There were also significant numbers of immigrants from Canada, Mexico, and other parts of Latin America. In addition, there were smaller numbers of immigrants from Asia and other parts of the world.
+
+                Question: What does inioluwa mean,
+                Answer: Inioluwa is a Nigerian name that means \"God's gift\" in Yoruba, a language spoken in Nigeria. It is typically given to a child who is seen as a blessing from God.
+
+                Question: what is CVD,
+                Answer: CVD stands for Cardiovascular Disease. It is a group of conditions that affect the heart and blood vessels. These conditions include coronary artery disease (which can lead to heart attacks),
                 
-                Q: What does Inioluwa mean?
-                A: Inioluwa is a Nigerian (Yoruba) name meaning "God's gift". It is usually given to a child seen as a blessing from God.
-                
-                Q: What is CVD?
-                A: CVD stands for Cardiovascular Disease. It refers to conditions affecting the heart and blood vessels, such as coronary artery disease.
-                
-                Q: {text}
-                A: Let's think step by step before answering."""
+                Now answer clearly, exactly the following: {text}.
+                Answer: Let's think step by step before answering."""
 
 
     @overrides()
     def few_shots_CoT_SC(self, text, num_samples=5, max_len=50, do_print=False):
         prompt = self.few_shots_CoT(text)
         samples = self.functions.self_consistency(prompt, num_samples, max_len)
-        best_answer, all_votes = self.functions.majority_vote(samples)
         if do_print:
             st.markdown("### 📝 Answers (Self-consistency)")
-
-            st.markdown("**All Sampled Answers:**")
-            st.code("\n".join(samples), language="text")
-
-            st.markdown("**Self-consistent Answer:**")
-            st.code(best_answer, language="text")
-
             st.markdown("**All Votes:**")
-            st.code(str(all_votes), language="text")
-        return best_answer, all_votes
-
-    def run(self, text, do_print=False, type='Direct zero-shot', type_output = None, num_samples=5, max_len=100):
+            st.code(samples, language="text")
+        return samples
+    @overrides()
+    def few_shots_ToT(self, text):
+      st.code('Simple QA do not need complicated ToT !',language="text")
+    @overrides()
+    def zero_shot_ToT(self, text):
+      st.code('Simple QA do not need complicated ToT !',language="text")
+    def run(self, text, do_print=False, type='Direct zero-shot', type_output = None, num_samples=5, max_len=200):
         prompt = None
         if type == 'Zero-shot CoT + Self-consistency':
             self.zero_shot_CoT_SC(text, num_samples, max_len, do_print)
@@ -99,19 +99,16 @@ class QA_knowledge(Pattern):
                 prompt = self.zero_shot_CoT(text)
             elif type == 'Zero-shot CoT + Self-consistency':
                 self.zero_shot_CoT_SC(text, num_samples, max_len, do_print)
-            elif type == 'Zero-shot ToT':
-                 print('Simple QAs arent suitable with ToT')
-                 return 0
             elif type == 'Direct few-shots':
                 prompt = self.few_shots_direct(text)
             elif type == 'Few-shots CoT':
                 prompt = self.few_shots_CoT(text)
-            elif type == 'Few-shots ToT':
-                print('Simple QAs arent suitable with ToT')
-                return 0
-            input = self.tokenizer(prompt, return_tensors='pt').to('cuda')
-            output = self.functions.generate_output(type=type_output, input=input, max_len=max_len)
+                max_len = 300
             if do_print:
                 st.markdown("### Prompt:")
                 st.code(prompt, language="text")
-            return self.tokenizer.decode(output[0], skip_special_tokens=True)
+            input = self.tokenizer(prompt, return_tensors='pt', return_attention_mask=False).to('cuda')
+            input_len = input['input_ids'].shape[1]
+            output = self.functions.generate_output(type=None, input=input, max_len=max_len)
+            generate_out = output[0][input_len:]
+            return self.tokenizer.decode(generate_out, skip_special_tokens=True)
