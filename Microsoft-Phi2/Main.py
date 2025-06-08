59b2cb441d1275fc3427aba7da6b86f77c7a68b0
bokuwakira1411 <158143866+bokuwakira1411@users.noreply.github.com>
2025-06-08 12:11:53 +0700

    Update Main.py

--- Microsoft-Phi2/Main.py
+++ Microsoft-Phi2/Main.py
@@ -2,6 +2,7 @@ from basic_prompt_designs.Classification import Classification
 from basic_prompt_designs.Computation import Computation
 from basic_prompt_designs.QA_knowledge import QA_knowledge
 from basic_prompt_designs.Reasoning import Reasoning
+from basic_prompt_designs.General import General
 class Main:
     def __init__(self, tokenizer, model, name_task, type_prompt=None, comp_model=None, X=None, vectorizer=None, task_lib=None):
         self.name_task = name_task
@@ -12,6 +13,8 @@ class Main:
             self.task = Computation(tokenizer, model, comp_model, X, vectorizer, task_lib)
         elif name_task=='reasoning':
             self.task = Reasoning(tokenizer, model, comp_model, X, vectorizer, task_lib)
+        elif name_task=='general':
+            self.task = General(tokenizer, model, comp_model, X, vectorizer, task_lib)
         else:
             self.task = QA_knowledge(tokenizer, model, comp_model, X, vectorizer, task_lib)
     def main(self, text, name_prompt, do_print=False):
